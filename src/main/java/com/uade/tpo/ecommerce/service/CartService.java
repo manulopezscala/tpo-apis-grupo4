815836cf4d3d2a04ad63ad3a75ab5c488a4dd7c7
Manu <manuel.lopezscala@gmail.com>
2026-03-19 17:22:53 -0300

    Correccion de errores

--- src/main/java/com/uade/tpo/ecommerce/service/CartService.java
+++ src/main/java/com/uade/tpo/ecommerce/service/CartService.java
@@ -10,8 +10,5 @@ public interface CartService {
 
     Optional<Cart> getCartById(int cartId);
 
-import com.uade.tpo.ecommerce.entity.Cart;
-
-public interface CartService {
     Cart createCart(String userId, String currency);
 }
