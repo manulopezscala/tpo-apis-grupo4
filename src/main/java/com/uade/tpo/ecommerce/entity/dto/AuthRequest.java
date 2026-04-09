de9b846725f4ca564db5081aa2de8d2bce6c2cf1
Galo Guardia <galoguardia@gmail.com>
2026-04-09 20:51:26 -0300

    feat: add role authorization setup

---
+++ src/main/java/com/uade/tpo/ecommerce/entity/dto/AuthRequest.java
@@ -0,0 +1,4 @@
+package com.uade.tpo.ecommerce.entity.dto;
+
+public record AuthRequest(String username, String password) {
+}
---
+++ src/main/java/com/uade/tpo/ecommerce/entity/dto/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.uade.tpo.ecommerce.entity.dto;
+
+public record AuthResponse(String token) {
+}
