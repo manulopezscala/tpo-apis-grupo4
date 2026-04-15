f44289ddb62dc7aae5bfa2b9fd2f5b40c5120ed0
Galo Guardia <galoguardia@gmail.com>
2026-04-15 01:57:40 -0300

    fix: cortar referencias inversas en serialización JSON para evitar el bucle

--- src/main/java/com/uade/tpo/ecommerce/entity/CartItem.java
+++ src/main/java/com/uade/tpo/ecommerce/entity/CartItem.java
@@ -1,5 +1,6 @@
 package com.uade.tpo.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -19,6 +20,8 @@ public class CartItem {
     private Double unitPrice;
 
     // RELACIÓN CON CART
+    // Usamos WRITE_ONLY para aceptar cart en el request pero no devolverlo en el response (evita bucles JSON)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToOne
     @JoinColumn(name = "cart_id")
     private Cart cart;
